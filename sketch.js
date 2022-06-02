a292cb318a2dcd53194e89b1290c6330c5e88600
saahiln06 <106373235+saahiln06@users.noreply.github.com>
2022-06-02 14:17:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,65 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
 
-function setup() {
-  createCanvas(400,400);
-  background(51);
-  box = createSprite(200,200,30,30);
-
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
-function draw() 
-{
-
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    background("red");
-    
-  }
-  // write the code to change background color 
-  // to red when RIGHT_ARROW is pressed
+function setup(){
   
+  createCanvas(400,400);
+  
+// Moving background
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+
+//creating boy running
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
   
 
-  if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
-    
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
-   
-  }
+leftBoundary=createSprite(0,0,100,800);
 
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
+//leftBoundary.invisible = false;
+ //leftBoundary.visible = true;
+// leftBoundary.invisible = true;
+ leftBoundary.visible = false;
 
 
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+  
+  //code to reset the background
+
+  /*if(path.y > 400 ){
+    path.y = height/2;
+  }*/
+
+  if(path.y > 400 ){
+   path.y = height/2;
+  }
+
+  /*if(path.y > 400 ){
+path.y = height/2;}*/
+
+/*if(path.y > 400 ){path.y = height/2;}*/
   
   drawSprites();
 }
-
